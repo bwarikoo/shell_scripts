54165dc9ece87714cc9604b052da654680b38c59
bakul warikoo <bwarikoo@gmail.com>
2023-07-11 17:39:45 -0700

    updated disk space script

--- diskspaceScript.sh
+++ diskspaceScript.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+threshold=15
+timestamp=$(date +'%d-%m-%Y %H:%M:%S')
 df -H | awk '{print $1 " " $5}' | while read output;
 do
     usage=$(echo $output | awk '{print $2}' | cut -d'%' -f1)
     file_system=$(echo $output | awk '{print $1}')
-    if [[ usage -lt 20 ]]
+    if [[ $usage -lt $threshold ]]
     then
-        echo "CRITICAl at FileSystem ${file_system}"
+        echo "CRITICAl for FileSystem ${file_system} at ${timestamp}"
     fi
 done
