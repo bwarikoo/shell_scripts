eaa9a8a512e941c3c5d7c4b443b7a1b1d4e08a96
Bakul Warikoo <32089999+bwarikoo@users.noreply.github.com>
2023-07-24 16:36:53 -0700

    Update backupData.sh

--- backupData.sh
+++ backupData.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Specify the path for the source and destination folder
 src_dir="<SourcePath>"
 tgt_dir="<DestinationPath>"
 
+# Declaring variables 
 current_timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
-#backup_folder=$tgt_dir/$current_timestamp.tgz
+backup_file=$tgt_dir/$current_timestamp.tgz
 
+# Actual Back up Operation
 echo "Taking Backup"
-tar czf $tgt_dir/$current_timestamp.tgz -C $src_dir .
+tar czf $backup_file -C $src_dir .
 echo "Successfully taken backup"
